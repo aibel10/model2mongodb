fa6c950cdccaf06e6cc2b521477bd5e434508819
aibel10 <150226390+aibel10@users.noreply.github.com>
2025-04-07 13:58:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-var express = require('express');
+var express=require('express');
 require("./db")
+var emp=require('./model/employee');
+var app =express();
+var port=3999;
 
-// init
-var app=express();
-var port=1000;
-var sModel=require('./model/sample')
-
-// mildware to add data to db
 app.use(express.json());
 
-// api to add data to db
-app.post('/a',(req,res)=>{
+app.post('/',(req,res)=>{
     try{
-        sModel(req.body).save();
+        emp(req.body).save();
         res.send("Data added")
     }catch(error){
         res.send("error")
     }    
 })
 
-app.get('/z',async(req,res)=>{
+app.get('/',async(req,res)=>{
     try{
-        var data= await sModel.find();
+        var data= await emp.find();
         res.send(data)
     }catch(error){
         res.send(error)
@@ -32,14 +28,23 @@ app.get('/z',async(req,res)=>{
 app.delete('/:id',async(req,res)=>{
     try{
         console.log(req.params.id)
-        await sModel.findByIdAndDelete(req.params.id);
+        await emp.findByIdAndDelete(req.params.id);
         res.send("Data deleted")
     }catch(error){
         res.send(error)
     }
 })
 
+app.put('/:id',async(req,res)=>{
+    try{
+        await emp.findByIdAndUpdate(req.params.id,req.body);
+        res.send("Data updated")
+        console.log(req.params.id)
+       }catch(error){
+        res.send(error)
+    }
+})
 
 app.listen(port,()=>{
-    console.log(`Server is up and running in ${port}`)
+    console.log('server is up and running in ${port}')
 })
